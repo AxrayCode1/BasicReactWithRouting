323bbd8d016f486c1e0aa260b92a0f99bc1e52e8
SHIHCHANG <axray@hotmail.com>
2020-04-14 14:14:09 +0800

    completed route with parameters

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Learning React Router
 
 1. npm install --save react-router react-router-dom
 2. Use Route & exact 連接的 URL 必須與 path 完全吻合才行
+3. Use Route with Mutil Page，當進到 Page 時會 Reload 整個 Page，Reload Page 會造成之前的state完全不見
+   a. 當不要 Reload 整個 page，需使用 React Router Dom 的 Link Component
+4. Use withRouter HOC component to get route props
+5. Relative Path
+   ex: <Link to={props.match.url + '/new'}>
+6. Use NavLink to style active link
+   a. Use activeClassName to change styel active classname
+   b. inline style with activeStyle
+7. Route with Parameters
+   ex: 
+       route  : Route path="/:id" exact component={FullPost} />
+       get id : this.props.match.params.id
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
-import Posts from './Posts/Posts';
 import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+
 
 class Blog extends Component {
-    render () {
-       
-
+    render () {       
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><NavLink 
+                                to="/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>               
                 {/* <Route path="/" exact render={() => <h1>Home</h1>}/>    
                 <Route path="/" render={() => <h1>Home 2</h1>}/> */}
-                <Route path="/" exact component={Posts} />
+                <Route path="/" exact component={Posts} />                
+                <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
